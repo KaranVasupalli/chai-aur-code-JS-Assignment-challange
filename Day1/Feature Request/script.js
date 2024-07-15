3b1ada99cfd7cf83345f6bb2be951ebaee6bc7b6
Karan Vasupalli <vkaran14210@gmail.com>
2024-07-15 11:03:56 +0530

    add

---
+++ Day1/Feature Request/script.js
@@ -0,0 +1,34 @@
+// 1. Variable Types console log: Write a script that declares variables of different data types and logs both the value and type of each variable to the console
+
+
+
+// let num = 1;
+// let str = "ashray";
+// let bool = true;
+// let obj = {id:1,userName:"vinay"}
+// let arr = [1,2,"suraj",true]
+
+// console.log(num);
+// console.log(typeof(num));
+// console.log(str);
+// console.log(typeof(str));
+// console.log(bool);
+// console.log(typeof(bool));
+// console.log(obj);     
+// console.log(typeof(obj)); 
+// console.log(arr);
+// console.log(typeof(arr));
+
+
+// 2. Reassignment Demo: Create a script that demonstrate the Differance in behavior between let and const when it comes to reassignment. 
+
+// let num1 = 2;
+// num1 = 3;
+// console.log(num1);
+
+// const num2 = 2;
+// num2 = 3;
+// console.log(num2);
+
+
+
---
+++ Day1/index.js
@@ -0,0 +1,61 @@
+//DAY1
+
+//Variable Declaration
+
+//Task1 : Declare a variable using var, assign it a number, and log the value to the console.
+
+// var num = 6;
+// console.log(num);
+
+
+//Task2 : Declare a variable using let, assign it a String, and log the value to the console
+
+// let userName = "mayur";
+// console.log(userName);
+
+
+// Activity 2 : Constant Declaration
+//Task3: Declare a variable using const, assign it a boolean value, and log the value to the console
+
+// const mayur = true;
+// console.log(mayur);
+
+
+// Activity 3 : Data Types
+//Task4: Create Variables of different data types(numbers,string,boolean,object,array) and log each variables type using the type of each variable to the console
+
+
+// let num = 1;
+// let str = "ashray";
+// let bool = true;
+// let obj = {id:1,userName:"vinay"}
+// let arr = [1,2,"suraj",true]
+
+// console.log(typeof(num));
+// console.log(typeof(str));
+// console.log(typeof(bool));
+// console.log(typeof(obj));
+// console.log(typeof(arr));
+
+
+
+// Activity 4 : Reassigning Variables
+// Task 5 : Declare a variable using let, assign it an initail value, reassign a new value,and log both values to the console
+
+
+// let user ;
+// user="karan"
+// console.log(user);
+// user="mayur"
+// console.log(user);
+
+
+
+// Activity 5 : Understanding const
+// Task 6 : Try reassigning a variable declared with const and observe the error 
+
+
+// const user = "karan"
+// user = "Mayur"
+
+// error - TypeError: Assignment to constant variable.
