26d4f06198f93307fa9a2a92d11e82b0716ba807
Karan Vasupalli <vkaran14210@gmail.com>
2024-07-16 09:14:47 +0530

    adding Day 2

---
+++ Day2/Feature Request/Arithmatic.js
@@ -0,0 +1,15 @@
+// Arithmetic Operations 
+
+// write a script that performs that performs basic arithmetic operations(addition,substraction,multiplication,divison,remainder) on two numbers and logs the results.
+
+// let a = 10;
+// let b = 4;
+
+// console.log(a+b);   
+// console.log(a-b);  
+// console.log(a*b);  
+// console.log(a/b);  
+// console.log(a%b);  
+
+
+
---
+++ Day2/index.js
@@ -0,0 +1,169 @@
+// Day 2: Operators
+
+
+// Activity 1: Arithmetic Operation
+
+// // Task 1: Write a program to add two numbers and log the result to the console.
+
+// let a = 24;
+// let k = 30;
+
+// console.log(a + k);
+
+
+
+// Task 2: Write a program to substract two numbers and log the result to the console.
+
+// let a = 24;
+// let k = 30;
+
+// console.log(a - k);
+
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+
+// let a = 24;
+// let k = 30;
+
+// console.log(a * k);
+
+
+
+// Task 4: Write a program to multiply two numbers and log the result to the console.
+
+// let a = 24;
+// let k = 30;
+
+// console.log(a / k);
+
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console
+
+// let a = 8;
+// let k = 3;
+
+// console.log(a % k);
+
+
+
+
+
+
+
+
+
+
+
+// Activity 2: Assignment Operators
+
+// Task 6: Use the += operator to add a number to a variable and log the result to the console 
+
+
+// let a = 10;
+// let b = a+=6;
+// console.log(b);
+
+
+// Task 7: Use the += operator to substract a number to a variable and log the result to the console 
+
+
+// let a = 10;
+// a-=6;
+// console.log(a);
+
+
+
+
+// Activity 3: Comparison // return a boolean value true or false
+
+// Task 8: Write a program to compare two number using > and < and log the result to the console
+
+// let a = 9;
+// let b = 8;
+
+// console.log( a > b)
+
+// let a = 9;
+// let b = 8;
+
+// console.log( a < b)
+
+
+// Task 9: Write a program to compare two number using >= and <= and log the result to the console
+
+// let a = 9;
+// let b = 8;
+// console.log( a >= b)
+
+// let c = 9;
+// let d = 8;
+// console.log( a <= b)
+
+
+// Task 10: Write a program to compare two number using == and === and log the result to the console.
+
+// let a = "karan"
+// let b = "karan"
+
+// console.log(a==b);
+
+
+// let c = 2
+// let d = "2"
+
+// console.log(c===d);
+
+
+
+
+// Activity 4: Logical Operators
+
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+
+// let a = 9;
+// let b = 8;
+
+// if(a > b && a==b){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+
+// let a = 9;
+// let b = 8;
+
+// if(a > b || a == b){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+// Task 13: Write a program that uses the ! operator to negate a conditions and log the result to the console.
+
+// let a = 7;
+// if(!(a>2)){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+// Activity 5: Ternary operator
+
+// Task 14: write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console 
+
+// let a = 5;
+
+// let b = a > 0 ? "positive" : "negative"
+
+// console.log(b);
+
+
+
